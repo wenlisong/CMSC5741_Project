f399c4ea18eef6d63ca4012589b4bb179ca2cf50
JiazeWang <jiazewang@csu.edu.cn>
2018-12-04 22:08:46 +0800

    extract_fix_bug

--- extract/extract.py
+++ extract/extract.py
@@ -9,6 +9,6 @@ for photo in os.listdir(file_path):
     picture_of_me = face_recognition.load_image_file(path)
     if len(face_recognition.face_encodings(picture_of_me)):
          my_face_encoding = face_recognition.face_encodings(picture_of_me)[0]
-         np.savetxt(file_path % photo, my_face_encoding, fmt='%f')
+         np.savetxt('/research/pheng5/jzwang/big_data/project/feature/%s.txt' % photo, my_face_encoding, fmt='%f')
     print(count)
     count = count + 1
