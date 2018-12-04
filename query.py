f6e3d97d549800e8065d07ef72690f0f51997c26
yongbiaoai <yongbiaoai@gmail.com>
2018-12-04 20:51:24 +0800

    print top images' info
    Signed-off-by: yongbiaoai <yongbiaoai@gmail.com>

--- query.py
+++ query.py
@@ -1,9 +1,15 @@
 import numpy as np
 from lshash import LSHash
+import json
 
 feature_dir = "./feature/"
 sample = np.loadtxt(feature_dir + "1028975_1983-07-06_2012.jpg.txt")
 lsh = LSHash(16, 128, storage_config={"redis": {"host": 'localhost', "port": 6379, "db": 0}},
              matrices_filename="./matrices.npz")
-result = lsh.query(sample, 5)
-print(result)
+result = lsh.query(sample, num_results=10)
+
+# Print Top Similar Images
+for rank, item in enumerate(result, start=1):
+    img_path = json.loads(item[0])[1]
+    distance = item[1]
+    print("Top #%d similar image:%s, The distance is:%f" % (rank, img_path, distance))
