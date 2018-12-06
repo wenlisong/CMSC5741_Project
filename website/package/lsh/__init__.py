92927240d44f8098a11bdb4cbe05206fedcab408
yongbiaoai <yongbiaoai@gmail.com>
2018-12-06 18:21:05 +0800

    Revert "change dependency py directory"
    This reverts commit 15d25aba3b05e2552e3bf5b74d0a79096705fa93.

---
+++ website/package/lsh/__init__.py
@@ -0,0 +1,3 @@
+from lshash import LSHash
+from query import *
+from constants import *
--- website/similarity/views.py
+++ website/similarity/views.py
@@ -4,8 +4,8 @@ import json
 import os
 import face_recognition
 import numpy as np
-from lshash import *
-from constants import *
+from package.lsh.lshash import *
+from package.lsh.constants import *
 import pdb
 
 def index(request):
