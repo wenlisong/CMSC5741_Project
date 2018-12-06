2e46312312e9a24a97c7187bb3c273eb324a565c
songwenli <327361201@qq.com>
2018-12-07 00:48:53 +0800

    1. website frontend: add some style for beauty.

--- similarity/views.py
+++ similarity/views.py
@@ -17,7 +17,7 @@ def upload(request):
     elif request.method == "POST":
         img_obj = request.FILES.get('img')
         # save images
-        with open(os.path.join('static/images', img_obj.name), 'wb') as f:
+        with open(os.path.join('static/data/images', img_obj.name), 'wb') as f:
             for chunk in img_obj.chunks(chunk_size=1024):
                 f.write(chunk)
         #exract
@@ -40,7 +40,7 @@ def query(feature_path):
     for rank, item in enumerate(result, start=1):
         img_info = json.loads(item[0])
         # absolute path, remove '.txt' at end
-        img_path = 'static/images/{0}'.format(img_info[1].rstrip('.txt'))
+        img_path = '/static/data/images/{0}'.format(img_info[1].rstrip('.txt'))
         distance = item[1]
         images_info.append({'path': img_path, 'distance': distance, 'rank':rank})
     print(images_info)
@@ -48,13 +48,13 @@ def query(feature_path):
 
 def extract(image_name):
     cwd = os.getcwd() # CMSC5741_Project/website
-    img_directory_path = os.path.join(cwd, "static/images") # CMSC5741_Project/website/static/images
+    img_directory_path = os.path.join(cwd, "static/data/images") # CMSC5741_Project/website/static/data/images
     image_path = os.path.join(img_directory_path, image_name)
     image = face_recognition.load_image_file(image_path)  # load image
     features = face_recognition.face_encodings(image)
     if features:
         feature = features[0]
-        feature_path = cwd+'/static/feature/{0}.txt'.format(image_name)
+        feature_path = cwd+'/static/data/feature/{0}.txt'.format(image_name)
         np.savetxt(feature_path, feature, fmt='%f')
         return feature_path
     else:
