8731becf699df30eec3c371a48aa1211c856dc9e
songwenli <327361201@qq.com>
2018-12-06 16:34:16 +0800

    1.add lshash package to website 2.continue extract feature function 3.continue extract query function 4.frontend logic almost finish

---
+++ website/package/lsh/constants.py
@@ -0,0 +1,5 @@
+HASH_SIZE = 16
+INPUT_DIMS = 128
+NUMS_TABLE = 5
+STORAGE_CONFIG = {"redis": {"host": 'localhost', "port": 6379, "db": 0}}
+MATRICES_NAME = "static/matrices.npz"
--- website/similarity/views.py
+++ website/similarity/views.py
@@ -4,7 +4,8 @@ import json
 import os
 import face_recognition
 import numpy as np
-from lshash.lshash import LSHash
+from package.lsh.lshash import *
+from package.lsh.constants import *
 import pdb
 
 def index(request):
@@ -14,41 +15,46 @@ def upload(request):
     if request.method=="GET":
         return  render(request,'index.html')
     elif request.method == "POST":
-        res = {"status":"success","code":999}
         img_obj = request.FILES.get('img')
+        # save images
         with open(os.path.join('static/images', img_obj.name), 'wb') as f:
             for chunk in img_obj.chunks(chunk_size=1024):
                 f.write(chunk)
+        #exract
         feature_path = extract(img_obj.name)
-        print(feature_path)
         if feature_path:
-            # query(feature_path)
-            res['upload_img'] = [os.path.join('/static/images', img_obj.name)] * 10
+            res = {"status": "success"}
+            query_results = query(feature_path)
+            res['query_results'] = query_results
         else:
-            res['status'] = "fail"
+            res = {"status": "fail"}
         return JsonResponse(res)
 
 def query(feature_path):
-    sample = np.loadtxt(feature_path)
-    lsh = LSHash(16, 128, storage_config={"redis": {"host": 'localhost', "port": 8080, "db": 0}},
-             matrices_filename="./matrices.npz")
-    result = lsh.query(sample, num_results=10)
+    feature = np.loadtxt(feature_path)
+    lsh = LSHash(HASH_SIZE, INPUT_DIMS, num_hashtables=NUMS_TABLE, storage_config=STORAGE_CONFIG,
+             matrices_filename=MATRICES_NAME)
+    result = lsh.query(feature, num_results=5, distance_func="norm")
     # Print Top Similar Images
+    images_info = []
     for rank, item in enumerate(result, start=1):
-        img_path = json.loads(item[0])[1]
+        img_info = json.loads(item[0])
+        img_path = img_info[1]
         distance = item[1]
-        print("Top #%d similar image:%s, The distance is:%f" % (rank, img_path, distance))
+        images_info.append({'path': img_path, 'distance': distance, 'rank':rank})
+    return images_info
+    
 
 def extract(image_name):
-    cwd = os.getcwd()
-    file_path = os.path.join(cwd, "static/images")
-    #extract features from file_path
-    image_path = os.path.join(file_path, image_name)
-    picture_of_me = face_recognition.load_image_file(image_path)
-    if len(face_recognition.face_encodings(picture_of_me)):
-        my_face_encoding = face_recognition.face_encodings(picture_of_me)[0]
+    cwd = os.getcwd() # CMSC5741_Project/website
+    img_directory_path = os.path.join(cwd, "static/images") # CMSC5741_Project/website/static/images
+    image_path = os.path.join(img_directory_path, image_name)
+    image = face_recognition.load_image_file(image_path)  # load image
+    features = face_recognition.face_encodings(image)
+    if features:
+        feature = features[0]
         feature_path = cwd+'/static/feature/{0}.txt'.format(image_name)
-        np.savetxt(feature_path, my_face_encoding, fmt='%f')
+        np.savetxt(feature_path, feature, fmt='%f')
         return feature_path
     else:
         return False
